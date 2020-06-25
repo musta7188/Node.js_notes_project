060af6d84e57a2efc15ef86fc6ae1ab9468c8dd1
musta7188 <musta7188@hotmail.it>
2020-06-25 13:13:25 +0100

    see current data body

--- Asynchronous/weather-app/app.js
+++ Asynchronous/weather-app/app.js
@@ -2,11 +2,11 @@ const request = require('request')
 
 const url  = 'http://api.weatherstack.com/current?access_key=c63d78724e7091670825c7985ba25fcb&query=37.8267,-122.4233'
 
-request({ url: url}, (error, response) => {
+request({ url: url, json: true}, (error, response) => {
 
-const data =  JSON.parse(response.body)
 
-console.log(data.current)
+
+   console.log(response.body.current)
 
 
 })
