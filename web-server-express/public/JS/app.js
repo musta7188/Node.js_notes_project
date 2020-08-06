ca86435381eaf347573b33f8f6274f7a6809e722
musta7188 <musta7188@hotmail.it>
2020-08-06 16:25:54 +0100

    restored from error

--- web-server-express/public/JS/app.js
+++ web-server-express/public/JS/app.js
@@ -1,24 +1,13 @@
-const e = require("express");
+console.log('javascript file is loaded ')
 
-console.log("javascript file is loaded ");
-
-fetch("http://puzzle.mead.io/puzzle")
-  .then((resp) => resp.json())
-  .then((data) => console.log(data));
+fetch('http://puzzle.mead.io/puzzle').then(resp => resp.json())
+.then(data => console.log(data))
 
 fetch("http://localhost:3000/weather?address=!")
-  .then((resp) => resp.json())
-  .then((data) => {
-    if (data.error) {
-      console.log("error:", data.error);
-    }
-    console.log(data)
-  });
-
-
-  const  weatherForm  = document.querySelector('form')
-
-  weatherForm.addEventListener('submit', (e) =>{
-    e.preventDefault()
-    console.log("testong")
-  })
+.then(resp => resp.json())
+.then(data => {
+  if(data.error){
+   console.log("error:", data.error)
+  }
+  console.log(data)
+})
