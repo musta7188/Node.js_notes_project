f7ec7e4a62b9ab7e2d62d118eff90ca4b52a03fb
musta7188 <musta7188@hotmail.it>
2020-10-10 12:33:22 +0100

    check if new note are duplicated

--- notes-app/notes1.js
+++ notes-app/notes1.js
@@ -1,20 +1,50 @@
-const fs = require('fs')
+const fs = require("fs");
 
 const getNotes = function () {
-   return 'saved notes'
-}
+  return "saved notes";
+};
 
+//load the notes
 const addNote = function (title, body) {
-  const notes = loadNotes()
-}
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((note) =>{
+     return note.title === title
+  })
 
-const loadNotes = function(){
-    const dataBuffer = fs.readFileSync('notes.json')
-    const dataJSON  = dataBuffer.toString()
-    return JSON.parse(dataJSON)
-}
+  //changed them
+
+  if(duplicateNotes === 0) {
+    notes.push({
+    title,
+    body,
+     });  
+  saveNotes(notes);
+  } else {
+    console.log('Note title already exists')
+  }
+  
+
+  ///save notes
+
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+///it will lead the notes inside the file, if there is not file will create and empty array and returned
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
 
 module.exports = {
   getNotes,
-  addNote
-}
+  addNote,
+};
